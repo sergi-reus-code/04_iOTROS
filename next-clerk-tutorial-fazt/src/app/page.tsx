8b60b7e5c4fd403cba5ddc9c65d739c1b0c9dea9
sergi-reus-code <sergi@reusconstruccions.com>
2024-01-13 08:08:17 +0100

    sdfgsdfg

--- next-clerk-tutorial-fazt/src/app/page.tsx
+++ next-clerk-tutorial-fazt/src/app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 
 export default function Page() {
@@ -14,6 +25,19 @@ export default function Page() {
       <Link href="/dashboard">click to dashboard</Link>
       
       
+      <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+
+
+
       </div>
     
   )
