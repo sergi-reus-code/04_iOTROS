13f33a7be35e0007275118b9e8e1b5c94a120ea9
sergi-reus-code <sergi@reusconstruccions.com>
2024-01-13 08:00:23 +0100

    xcbxcvb

--- next-clerk-tutorial-fazt/src/app/page.tsx
+++ next-clerk-tutorial-fazt/src/app/page.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+
+
 export default function Page() {
   return (
-    <div>page</div>
+    <div>
+      
+      <h1>Home Page</h1>
+
+      <Button variant="outline" size="sm">Click me</Button>
+      
+      <Link href="/dashboard">click to dashboard</Link>
+      
+      
+      </div>
+    
   )
 }
